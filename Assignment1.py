ae99b1830237de3a2c70cbb6ec8f5e2382745ed9
siddhantmisal50 <siddhantmisal50@gmail.com>
2022-05-24 07:26:24 +0530

    practical

--- Assignment1.py
+++ Assignment1.py
@@ -1,40 +1,46 @@
-#Consider a telephone book database of N clients. Make use of a hash table
-#implementation to quickly look up client‘s telephone number. Make use of two collision
-#handling techniques and compare them using number of comparisons required to find a set of
-#telephone numbers (Python)
+# Consider a telephone book database of N clients. Make use of a hash table
+# implementation to quickly look up client‘s telephone number. Make use of two collision
+# handling techniques and compare them using number of comparisons required to find a set of
+# telephone numbers (Python)
 
-#Algorithm:
-#1: Create a hash table with maximum size.
-#2: Define Hash function.
-#3: Read the telephone no & user’s information to insert it into hash table.
-#4: Insert telephone no value in the hash table.
-#5: If collision occurs, apply separate chaining method.
-#6: If user requires inserting another data, go to step4.
-#7: Display the all user’s data.
-#8: Read the telephone no from user to be found.
-#9: find the data from hash table.
-#10: Display the data with minimum no. of comparisons.
+# Algorithm:
+# 1: Create a hash table with maximum size.
+# 2: Define Hash function.
+# 3: Read the telephone no & user’s information to insert it into hash table.
+# 4: Insert telephone no value in the hash table.
+# 5: If collision occurs, apply separate chaining method.
+# 6: If user requires inserting another data, go to step4.
+# 7: Display the all user’s data.
+# 8: Read the telephone no from user to be found.
+# 9: find the data from hash table.
+# 10: Display the data with minimum no. of comparisons.
 
 def hash(num) -> int:
     return int(num % total_numbers)
+
+
 def linearProbingWithoutReplacement(h) -> int:
-    if(h==total_numbers-1):
+    if(h == total_numbers-1):
         h = 0
         return(h)
-    else:        
+    else:
         return(h+1)
+
+
 def search(num) -> bool:
-    if(hashTable[num]!=(None, None)):
+    if(hashTable[num] != (None, None)):
         return True
     else:
         return False
+
+
 def insert(name, num) -> int:
     hashed_num = hash(num)
-    if(search(hashed_num)): 
+    if(search(hashed_num)):
         hashed_num = hash(num)
         counter = 0
         while(search(hashed_num)):
-            if(counter==total_numbers-1):
+            if(counter == total_numbers-1):
                 print("Hash Table is full!")
             hashed_num = linearProbingWithoutReplacement(hashed_num)
             counter += 1
@@ -44,21 +50,26 @@ def insert(name, num) -> int:
         hashed_num = hash(num)
         hashTable[int(hashed_num)] = (name, num)
         return 1
+
+
 def displayHashTable() -> None:
     print(f"\nEntries : {total_numbers}")
     i = 0
     for bucket in hashTable:
         print(f"\t{i} : {bucket[0]} - {bucket[1]}")
         i += 1
+
+
 def isFull() -> bool:
     count = 0
     for bucket in hashTable:
-        if(bucket[0]!=None):
+        if(bucket[0] != None):
             count += 1
-    if(count==total_numbers):
+    if(count == total_numbers):
         return True
     else:
         return False
+
 
 while(True):
     total_numbers = int(input("Enter total numbers :"))
@@ -68,24 +79,25 @@ while(True):
 hashTable = [(None, None)]*total_numbers
 
 while(True):
-    choice = input("Enter Choice :\n1. Insert new number\n2. Display\n3. Exit\n-->")
+    choice = input(
+        "Enter Choice :\n1. Insert new number\n2. Display\n3. Exit\n-->")
     try:
         match int(choice):
-            case 1 :
+            case 1:
                 if(isFull()):
                     print("Hash Table is full!")
                 else:
                     name = input("Name : ")
                     while(True):
                         number = int(input("Num : "))
-                        if(number<=999999999):
+                        if(number <= 999999999):
                             print("Invalid Number! Please re-enter!")
                         else:
                             break
                     insert(name, number)
-            case 2 : 
+            case 2:
                 displayHashTable()
-            case 3: 
+            case 3:
                 exit()
     except:
         print("Enter valid choice.")
--- Assignment2.py
+++ Assignment2.py
@@ -1,81 +1,86 @@
-#PS = Implement all the functions of a dictionary (ADT) using hashing and handle
-#collisions using chaining with / without replacement. Data: Set of (key, value) pairs, Keys are mapped to
-#values, Keys must be comparable, Keys must be unique Standard Operations: Insert(key, value), Find(key),
+# PS = Implement all the functions of a dictionary (ADT) using hashing and handle
+# collisions using chaining with / without replacement. Data: Set of (key, value) pairs, Keys are mapped to
+# values, Keys must be comparable, Keys must be unique Standard Operations: Insert(key, value), Find(key),
 #Delete(key) (python)
 
-#Insert Operation : Whenever an element is to be inserted, compute the hash code of the key passed and
-#locate the index using that hash code as an index in the array. Use linear probing for empty location, if an
-#element is found at the computed hash code.
-#Example void insert(int key,int data)
+# Insert Operation : Whenever an element is to be inserted, compute the hash code of the key passed and
+# locate the index using that hash code as an index in the array. Use linear probing for empty location, if an
+# element is found at the computed hash code.
+# Example void insert(int key,int data)
 # {struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
-#item->data = data;
-#item->key = key;
-#//get the hash int hashIndex = hashCode(key);
-#//move in array until an empty or deleted cell
-#while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1)
-#{ //go to next cell ++hashIndex; //wrap around the table hashIndex %= SIZE; }
-#hashArray[hashIndex] = item; }
+# item->data = data;
+# item->key = key;
+# //get the hash int hashIndex = hashCode(key);
+# //move in array until an empty or deleted cell
+# while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1)
+# { //go to next cell ++hashIndex; //wrap around the table hashIndex %= SIZE; }
+# hashArray[hashIndex] = item; }
 
-#Delete Operation: Whenever an element is to be deleted, compute the hash code of the key passed and
-#locate the index using that hash code as an index in the array. Use linear probing to get the element ahead
-#if an element is not found at the computed hash code. When found, store a dummy item there to keep the
-#performance of the hash table intact.
-#Example struct DataItem* delete(struct DataItem* item)
-#{ int key = item->key;
-#//get the hash int hashIndex = hashCode(key);
+# Delete Operation: Whenever an element is to be deleted, compute the hash code of the key passed and
+# locate the index using that hash code as an index in the array. Use linear probing to get the element ahead
+# if an element is not found at the computed hash code. When found, store a dummy item there to keep the
+# performance of the hash table intact.
+# Example struct DataItem* delete(struct DataItem* item)
+# { int key = item->key;
+# //get the hash int hashIndex = hashCode(key);
 #//move in array until an empty#
-#while(hashArray[hashIndex] !=NULL)
-#{ if(hashArray[hashIndex]->key == key)
-#{ struct DataItem* temp = hashArray[hashIndex];
-#//assign a dummy item at deleted position hashArray[hashIndex] = dummyItem;
-#return temp; }
-#//go to next cell ++hashIndex;
-#//wrap around the table hashIndex %= SIZE; } return NULL; }
+# while(hashArray[hashIndex] !=NULL)
+# { if(hashArray[hashIndex]->key == key)
+# { struct DataItem* temp = hashArray[hashIndex];
+# //assign a dummy item at deleted position hashArray[hashIndex] = dummyItem;
+# return temp; }
+# //go to next cell ++hashIndex;
+# //wrap around the table hashIndex %= SIZE; } return NULL; }
 
-#Search Operation Whenever an element is to be searched, compute the hash code of the key passed and
-#locate the element using that hash code as index in the array. Use linear probing to get the element ahead if
-#the element is not found at the computed hash code.
-#Example
-#struct DataItem *search(int key)
-#{
-#//get the hash int hashIndex = hashCode(key); //move in array until an empty while(hashArray[hashIndex]
+# Search Operation Whenever an element is to be searched, compute the hash code of the key passed and
+# locate the element using that hash code as index in the array. Use linear probing to get the element ahead if
+# the element is not found at the computed hash code.
+# Example
+# struct DataItem *search(int key)
+# {
+# //get the hash int hashIndex = hashCode(key); //move in array until an empty while(hashArray[hashIndex]
 #!= NULL)
-#{
-#if(hashArray[hashIndex]->key == key)
-#return hashArray[hashIndex];
-#/go to next cell ++hashIndex;
-#//wrap around the table hashIndex %= SIZE;
-#}
-#return NULL; }
+# {
+# if(hashArray[hashIndex]->key == key)
+# return hashArray[hashIndex];
+# /go to next cell ++hashIndex;
+# //wrap around the table hashIndex %= SIZE;
+# }
+# return NULL; }
 
 class HashTable:
     def __init__(self, num) -> None:
         self.size = int(num)
         self.dictionary = [[None]]*num
+
     def isEmpty(self) -> str:
         for x in self.dictionary:
-            if x[0] == None :
+            if x[0] == None:
                 pass
             else:
                 return "List is not empty!"
         return "List is empty!"
+
     def hash(self, key) -> int:
         return key % self.size
+
     def search(self, key, value) -> str:
         if value in self.dictionary[key]:
             return True
         else:
             return False
+
     def insert(self, key, value) -> bool:
         key = self.hash(key)
         if(self.search(key, value)):
             return False
         else:
-            if ((len(self.dictionary[key]) == 1) and (self.dictionary[key][0]==None)) :
+            if ((len(self.dictionary[key]) == 1) and (self.dictionary[key][0] == None)):
                 self.dictionary[key] = [value]
             else:
                 self.dictionary[key].append(value)
             return True
+
     def display(self) -> None:
         print(self.dictionary)
 
@@ -87,36 +92,38 @@ if __name__ == '__main__':
         except:
             print("Enter an integer!")
             continue
-        if(num<=0):
+        if(num <= 0):
             print("Enter non-zero positive number!")
-        elif(num>0):
+        elif(num > 0):
             break
     mydictionary = HashTable(num)
     while(1):
         try:
-            choice = int(input("\nEnter :\n1. Insert\n2. Display\n3. Search\n4. Exit\n-->"))
+            choice = int(
+                input("\nEnter :\n1. Insert\n2. Display\n3. Search\n4. Exit\n-->"))
         except:
             print("Enter an integer!")
             continue
         match choice:
-            case 1 : 
+            case 1:
                 key = int(input("Enter key :"))
                 value = input("Enter value :")
                 state = mydictionary.insert(key=key, value=value)
-                print("Inserted successfully!") if state else print("Element is already present")
-            case 2 :
+                print("Inserted successfully!") if state else print(
+                    "Element is already present")
+            case 2:
                 mydictionary.display()
-            case 3 :
+            case 3:
                 key = int(input("Enter key : "))
                 value = input("Enter value :")
                 if(mydictionary.search(hash(key), value)):
                     print("Element is present!")
                 else:
                     print("Element is not present!")
-            case 4 :
+            case 4:
                 del mydictionary, key, value
                 print("Object has been deleted!")
                 print("Exiting Program...")
                 exit()
-            case _ :
+            case _:
                 print("Enter valid option!")
